e918af33f037b4f5516fa3b1c8249a9d6826c92c
xuyuxin-star <1452021693@qq.com>
2020-05-31 18:08:41 +0800

    修改注释

--- day5_29_loginmvp/src/main/java/com/example/day5_29_loginmvp/callback/LoginCallBack.java
+++ day5_29_loginmvp/src/main/java/com/example/day5_29_loginmvp/callback/LoginCallBack.java
@@ -3,6 +3,10 @@ package com.example.day5_29_loginmvp.callback;
 import com.example.day5_29_loginmvp.bean.LoginBean;
 
 public interface LoginCallBack {
+    /**
+     * aaaaa
+     * @param loginBean
+     */
     void onSuccess(LoginBean loginBean);
     void onFail(String error);
 }
